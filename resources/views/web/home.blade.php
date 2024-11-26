1c560c8161dd0448a9cbebfed30db305db94a890
Mariel Goc-ong <158608475+MarielGocong@users.noreply.github.com>
2024-11-26 13:57:04 +0800

    update

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -351,7 +351,7 @@ class="bg-salonPurple text-center text-neutral-100 lg:text-left">
     <a href="#!" class="mr-6 text-white dark:text-neutral-200">
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        class="h-6 w-6"
+        class="h-4 w-4"
         fill="currentColor"
         viewBox="0 0 24 24">
         <path
