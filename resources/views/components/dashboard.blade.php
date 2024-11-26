b8e601ff65bd3875bd996e39af7d4b393f80e4e5
Mariel Goc-ong <158608475+MarielGocong@users.noreply.github.com>
2024-11-26 14:21:18 +0800

    Update dashboard.blade.php

--- resources/views/components/dashboard.blade.php
+++ resources/views/components/dashboard.blade.php
@@ -133,7 +133,7 @@
 
                           <li class="px-5">
                             <div class="flex flex-row items-center h-8">
-                              <div class="text-sm font-light tracking-wide text-gray-500">INVENTORY MANAGING</div>
+                              <div class="text-sm font-light tracking-wide text-gray-500">INVENTORY MANAMENT</div>
                             </div>
                           </li>
 
