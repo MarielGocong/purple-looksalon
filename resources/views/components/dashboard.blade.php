c50ad1692352e4810bd88b757a562a95f16c18ea
Mariel Goc-ong <158608475+MarielGocong@users.noreply.github.com>
2024-11-26 15:48:35 +0800

    Update dashboard.blade.php

--- resources/views/components/dashboard.blade.php
+++ resources/views/components/dashboard.blade.php
@@ -133,19 +133,7 @@
                             </div>
                           </li>
 
-                          <li>
-                            <a href="{{ route('manageequipments') }}" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-purple-500 pr-6
-                            {{ request()->is('dashboard/manage/equipments') ? 'bg-gray-100 border-purple-500 text-gray-900 font-semibold' : '' }}
-                            ">
-
-                            <span class="inline-flex justify-center items-center ml-4">
-                              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="m7.848 8.25 1.536.887M7.848 8.25a3 3 0 1 1-5.196-3 3 3 0 0 1 5.196 3Zm1.536.887a2.165 2.165 0 0 1 1.083 1.839c.005.351.054.695.14 1.024M9.384 9.137l2.077 1.199M7.848 15.75l1.536-.887m-1.536.887a3 3 0 1 1-5.196 3 3 3 0 0 1 5.196-3Zm1.536-.887a2.165 2.165 0 0 0 1.083-1.838c.005-.352.054-.695.14-1.025m-1.223 2.863 2.077-1.199m0-3.328a4.323 4.323 0 0 1 2.068-1.379l5.325-1.628a4.5 4.5 0 0 1 2.48-.044l.803.215-7.794 4.5m-2.882-1.664A4.33 4.33 0 0 0 10.607 12m3.736 0 7.794 4.5-.802.215a4.5 4.5 0 0 1-2.48-.043l-5.326-1.629a4.324 4.324 0 0 1-2.068-1.379M14.343 12l-2.882 1.664" />
-                              </svg>
-                            </span>
-                            <span class="ml-2 text-sm tracking-wide truncate">Manage Equipment</span>
-                            </a>
-                          </li>
+                         
 
                           <li>
                             <a href="{{ route('managesupplies') }}" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-purple-500 pr-6
